0f47642bec069df30c630f1d96bc04d0cd1e646a
허준수 <gjwnstn515@naver.com>
2025-01-19 18:43:03 +0900

    section5 완료

--- core/src/main/java/hello/core/order/AppConfig.java
+++ core/src/main/java/hello/core/order/AppConfig.java
@@ -2,26 +2,35 @@ package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
-    public MemberService memberService(){
+    @Bean
+    public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
-    public OrderService orderService(){
+    @Bean
+    public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
-    public DiscountPolicy discountPolicy(){
+    @Bean
+    public DiscountPolicy discountPolicy() {
         return new FixDiscountPolicy();
+//        return new RateDiscountPolicy();
     }
 }
